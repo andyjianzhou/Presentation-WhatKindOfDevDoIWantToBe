0eef04a10e526a4f1af2b2191816994c8ae47e6b
Pandycool <74941296+andyjianzhou@users.noreply.github.com>
2023-02-11 00:34:49 +0000

    Fixed checkbox button and changed border colors

--- src/Forms/PetForm.js
+++ src/Forms/PetForm.js
@@ -6,13 +6,13 @@ function PetForm() {
       <p>Do you like cats or dogs?</p>
     
       <span>
-        <input type="checkbox" id="pet" name="pet" value="Cat" />
-        <label htmlFor="pet">Cats 😻</label>
-        </span>
-        <span>
-        <br />
-        <input type="checkbox" id="pet" name="pet" value="Dog" />          
-        <label htmlFor="pet">Dogs 🐶</label>
+        <input type="checkbox" id="cat" name="pet" value="Cat" />
+        <label htmlFor="cat">Cats 😻</label>
+      </span>
+      <span>
+      <br />
+        <input type="checkbox" id="dog" name="pet" value="Dog" />          
+        <label htmlFor="dog">Dogs 🐶</label>
       </span>
     </div>
   );
